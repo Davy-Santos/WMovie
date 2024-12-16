3129649e8c4cde3ba97cd589f7216fb462b43da0
Davy-Santos <davy.santos06@gmail.com>
2024-12-16 17:57:21 -0300

    Update login.js

--- login.js
+++ login.js
@@ -5,7 +5,7 @@ function validateForm() {
     // Validação simples de exemplo
     if (usuario.length >= 1 ) {
         // Redirecionar para a página de sucesso
-        location.href = '../Assistir/html/assistir.html'; // Altere para a página desejada
+        location.href = 'Assistir/html/assistir.html'; // Altere para a página desejada
         return false; // Impede o envio do formulário
 }
 }
